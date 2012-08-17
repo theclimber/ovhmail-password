a2c92d4980e2cd05d84e7ce3b4707885c0377145
Gregoire de Hemptinne <greg@theclimber.be>
2012-08-17 11:44:35 +0200

    Adding licence

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+// ovhmail-password - Manage your OVH mails
+// Licence: http://www.opensource.org/licenses/zlib-license.php
+// Requires: php 5
+
 require_once(dirname(__FILE__).'/config.php');
 
 $errors = array();
