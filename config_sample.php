d6324f519bd13346e90d5eeb7813a8e0d33cc3e1
Gregoire de Hemptinne <greg@theclimber.be>
2012-08-26 12:22:16 +0200

    Adding changes of Simon

--- config_sample.php
+++ config_sample.php
@@ -1,7 +1,9 @@
 <?php
-$nic = "xxxxx-ovh"; // Votre nic ovh
-$pass = "********"; // Votre mot de passe ovh
-$domain = "yourdomaine.tld"; // your domain name
-$serveur="pop3.". $domain;
-$dom = "@". $domain;
+$nic = 'xxxxx-ovh'; // Votre nic ovh
+$pass = '********'; // Votre mot de passe ovh
+$domain = 'yourdomaine.tld'; // your domain name
+$serveur = "pop3.". $domain;
+$dom = '@'. $domain;
+ 
+define('DEBUG', 0);
 ?>
--- index.php
+++ index.php
@@ -2,166 +2,287 @@
 // ovhmail-password - Manage your OVH mails
 // Licence: http://www.opensource.org/licenses/zlib-license.php
 // Requires: php 5
-
-require_once(dirname(__FILE__).'/config.php');
-
-$errors = array();
-$passwordmail = '';
-$email_name = '';
-$email = '';
-$need_login = false;
-if (!empty($_POST) && isset($_POST['passwordmail']) && isset($_POST['email'])) {
-	$passwordmail = $_POST['passwordmail'] ;
-	$email_name = $_POST['email'];
-} else {
-	$need_login = true;
+ 
+require_once(__DIR__.'/config.php');
+ 
+error_reporting(DEBUG);
+ 
+$need_login = true;
+$form_error = array();
+ 
+function add_field_error($name, $message)
+{
+    global $form_error;
+ 
+    if (is_array($name)) {
+        $name = 'global_error_'.implode('_', $name);
+    }
+ 
+    if (!isset($form_error[$name])) {
+        $form_error[$name] = array();
+    }
+ 
+    $form_error[$name][] = $message;
 }
-$email = $email_name . $dom;
-$mbox = @imap_open('{'.$serveur.':143}INBOX', "$email", "$passwordmail");
-if (!$mbox) {
-	if (!empty($_POST)) {
-		$errors[] = "Authentication error";
-	}
-	$need_login = true;
-} else {
-	imap_close($mbox);
-	// Filtrage des données
-
-	foreach ($_REQUEST as $key => $val) {
-		$val = preg_replace("/[^_A-Za-z0-9-\.&=]/i",'', $val);
-		$_REQUEST[$key] = $val;
-	}
-
-	$newpass = '';
-	$newpass2 = '';
-	if (isset($_POST['newpass'])) {
-		$newpass = preg_replace("/[^_A-Za-z0-9-\.]/i",'', $_POST["newpass"]);
-	}
-	if (isset($_POST['newpass2'])) {
-		$newpass2 = preg_replace("/[^_A-Za-z0-9-\.]/i",'', $_POST["newpass2"]);
-	}
-
-	$success = '';
-	if (strlen($newpass) >= 8 && $newpass == $newpass2) {
-	// Vérification du bon nouveau mot de passe (avec les deux champs puis on valide si ok )
-		$soap = new SoapClient('https://www.ovh.com/soapi/soapi-1.2.wsdl');
-
-		//login
-		try {
-			$language = null;
-			$multisession = false;
-			$session = $soap->login($nic,$pass,$language,$multisession);
-			$success .= "login successfull<br/>";
-		} catch(SoapFault $fault) {
-			$errors[] = "Error : ".$fault;
-		}
-		//popModifyPassword
-		try {
-			$result = $soap->popModifyPassword($session, $domain, $email_name, $newpass, false);
-			$success .= "popModifyPassword successfull<br/>";
-			$success .= print_r($result);
-			$success .= "<br/>";
-			$success .= "<h3>Merci.<br />Mot de passe modifi&eacute;.</h3>";
-			$success .= "<h3>Il sera pris en compte d'ici une quinzaine de minutes</h3>";
-		} catch(SoapFault $fault) {
-			$errors[] = "Error : ".$fault;
-		}
-		//logout
-		try {
-			$result = $soap->logout($session);
-			$success .= "logout successfull<br/>";
-		} catch(SoapFault $fault) {
-			$errors[] = "Error : ".$fault;
-		}
-	} elseif (strlen($newpass) > 0 && $newpass != $newpass2) {
-	// ici le cas ou le premier nouveau mot de passe ne correspond pas au second
-		$errors[] = "The two passwords are not equal, please check it";
-	} elseif (strlen($newpass) > 0 && strlen($newpass) < 8) {
-	// Si le mot de passe fait moins de 8 caractères on refuse 
-		$errors[] = "Make sure your password has minimum 8 characters.";
-	}
+ 
+function has_field_error($name)
+{
+    global $form_error;
+ 
+    return !empty($form_error[$name]);
 }
-
-$error_text = '';
-if (!empty($errors)) {
-	$error_text .= '<ul class="error-list">';
-	foreach($errors as $err) {
-		$error_text .= '<li>'.$err.'</li>';
-	}
-	$error_text .= '</ul>';
+ 
+function get_field_error($name)
+{
+    global $form_error;
+ 
+    return has_field_error($name) ? $form_error[$name] : array();
 }
-
-if ($need_login) {
-	$title = "Welcome on OVH email management";
-} else {
-	$title = "Change the password of your email account";
+ 
+function get_field_errors($name)
+{
+    return get_field_error($name);
 }
+ 
+function is_valid_form()
+{
+    global $form_error;
+ 
+    return empty($form_error);
+}
+ 
+function valid_field($name, Closure $validator, $message, $http_method = 'POST')
+{
+    $datas = $http_method == 'POST' ? $_POST : $_GET;
+ 
+    $value = isset($datas[$name]) ? $datas[$name] : null;
+ 
+    if (false === $validator($value)) {
+        add_field_error($name, $message);
+    }
+}
+ 
+function valid_fields(array $fields, Closure $validator, $message, $http_method = 'POST')
+{
+    $datas = $http_method == 'POST' ? $_POST : $_GET;
+    $param = array();
+ 
+    foreach ($fields as $name) {
+        $param[$name] = isset($datas[$name]) ? $datas[$name] : null;
+    }
+ 
+    if (false === $validator($param)) {
+        add_field_error($fields, $message);
+    }
+}
+ 
+function is_global_error($name)
+{
+    return preg_match('/^global_error_/', $name);
+}
+ 
+function print_field_error($name)
+{
+    if (has_field_error($name)) {
+        echo '<ul class="error">';
+        foreach (get_field_error($name) as $error) {
+            echo sprintf('<li>%s</li>', htmlspecialchars($error));
+        }
+        echo '</ul>';
+    }
+}
+ 
+function print_global_errors()
+{
+    global $form_error;
+ 
+    $datas = array();
+ 
+    foreach ($form_error as $k => $v) {
+        if (is_global_error($k)) {
+            $datas[] = $v;
+        }
+    }
+ 
+    if (!empty($v)) {
+        echo '<ul class="error">';
+        foreach ($datas as $error) {
+            echo sprintf('<li>%s</li>', htmlspecialchars($error[0]));
+        }
+        echo '</ul>';
+ 
+    }
+}
+ 
+function filter_var_password($password)
+{
+    return preg_replace('/[^_A-Za-z0-9-\.&=]/i', '', $password);
+}
+ 
+if (!empty($_POST)) {
+    $need_login = false;
+ 
+    valid_field(
+        'email',
+        function($email) use ($dom) {
+            return filter_var($email.$dom, FILTER_VALIDATE_EMAIL);
+        },
+        'Vous devez indiquer un email valide.'
+    );
+ 
+    valid_field(
+        'passwordmail',
+        function($passwordmail) {
+            return trim($passwordmail) !== '';
+        },
+        'Vous devez indiquer un mot de passe.'
+    );
+ 
+    if (is_valid_form()) {
+        valid_fields(
+            array('email', 'passwordmail'),
+            function($datas) use ($dom, $serveur) {
+                $datas['email'].= $dom;
+ 
+                $imap_connection = @imap_open('{'.$serveur.':143}INBOX', $datas['email'], $datas['passwordmail']);
+ 
+                if ($imap_connection) {
+                    imap_close($imap_connection);
+ 
+                    return true;
+                }
+ 
+                return false;
+            },
+            'Les identifiants ne sont pas corrects.'
+        );
+ 
+        if (is_valid_form()) {
+            $form_error = array();
+ 
+            valid_field(
+                'newpass',
+                function($newpass) {
+                    $newpass = filter_var_password($newpass);
+ 
+                    return strlen($newpass) >= 8;
+                },
+                'Vous devez indiquer un mot de passe de 8 caractères minimum.'
+            );
+ 
+            valid_fields(
+                array('newpass', 'newpass2'),
+                function($datas) {
+                    return $datas['newpass'] === $datas['newpass2'];
+                },
+                'Les mots de passe doivent être identiques.'
+            );
+ 
+            if (is_valid_form()) {
+                try {
+                    $session = $soap->login($nic, $pass, $language = null, $multisession = false);
+                    $soap->popModifyPassword($session, $domain, $_POST['email'], filter_var_password($_POST['newpass']), false);
+                    $soap->logout($session);
+                } catch (SoapFault $fault) {
+                    $error = 'Echec : '.$fault->getMessage();
+                }
+            }
+        } else {
+            $need_login = true;
+        }
+    } else {
+        $need_login = true;
+    }
+}
+ 
+$title = $need_login ? 'Welcome on OVH email management' : 'Change the password of your email account';
+ 
+$passwordmail = isset($_POST['passwordmail']) ? htmlspecialchars($_POST['passwordmail']) : '';
+$email        = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+$newpass      = isset($_POST['newpass']) ? htmlspecialchars($_POST['newpass']) : '';
+$newpass2     = isset($_POST['newpass2']) ? htmlspecialchars($_POST['newpass2']) : '';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" title="normal" href="common.css">
-	<title>Change the password of your email account</title>
-	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <link rel="stylesheet" type="text/css" title="normal" href="common.css">
+    <title>Change the password of your email account</title>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
 </head>
 <body>
 <img id="logo" border="0" style="margin:0 11px" alt="OVH Mail" src="logo.jpg">
 <div id="login-form">
 <div class="boxtitle"><?php echo $title; ?></div>
-	<div class="boxcontent">
-<?php echo $error_text; ?>
-
-<?php
-if ($need_login) { ?>
-		<form name="form" action="index.php" method="post">
-			<table summary="" border="0">
-			<tbody><tr><td class="title">
-			<label for="email">Your email</label>
-			</td>
-			<td><input name="email" id="email" type="text" value="" />@<?php echo $domain; ?></td>
-			</tr>
-			<tr><td class="title"><label for="password">Your current password</label>
-			</td>
-			<td><input name="passwordmail" id="password" type="password" /></td>
-			</tr>
-			</tbody>
-			</table>
-
-			<p style="text-align:center;"><input type="submit" class="button mainaction" value="Authentication" /></p>
-		</form>
-
-<?php
-} elseif (strlen($success) > 0 && empty($errors)) {
-	print $success;
-} else { ?>
-
-		<form name="form "action="index.php" method="post">
-			<input type="hidden"  name="passwordmail"  value="<?php echo $passwordmail; ?>">
-			<input type="hidden"  name="email"  value="<?php echo $email_name; ?>">
-
-			<table summary="" border="0">
-			<tbody>
-			<tr><td colspan=2>
-				Your account:  <?php echo $email; ?><br /><br />
-			</td></tr>
-			<tr><td class="title">
-			<label for="newpass">Your new password</label>
-			</td>
-			<td><input name="newpass" id="newpass" type="password" /></td>
-			</tr>
-			<tr><td class="title">
-			<label for="newpass2">Confirm new password</label>
-			</td>
-			<td><input name="newpass2" id="newpass2" type="password" /></td>
-			</tr>
-			</tbody>
-			</table>
-
-			<p style="text-align:center;"><input type="submit" class="button mainaction" value="Send" /></p>
-		</form>
-<?php }
-?>
-	</div>
+    <div class="boxcontent">
+ 
+<?php if(!empty($error)): ?>
+    <p><?php echo htmlspecialchars($error); ?>
+<?php endif; ?>
+ 
+<?php print_global_errors(); ?>
+ 
+<?php if ($need_login): ?>
+    <form name="form" action="index.php" method="post">
+        <table summary="" border="0">
+            <tbody>
+                <tr>
+                    <td class="title">
+                        <label for="email">Your email</label>
+                    </td>
+                    <td>
+                        <input name="email" id="email" type="text" value="<?php echo $email; ?>" />@<?php echo $domain; ?>
+                        <?php print_field_error('email'); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="title">
+                        <label for="password">Your current password</label>
+                    </td>
+                    <td>
+                        <input name="passwordmail" id="password" value="<?php echo $passwordmail; ?>" type="password" />
+                        <?php print_field_error('passwordmail'); ?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+ 
+        <p style="text-align:center;"><input type="submit" class="button mainaction" value="Authentication" /></p>
+ 
+    </form>
+<?php else: ?>
+    <form name="form "action="index.php" method="post">
+        <input type="hidden"  name="passwordmail"  value="<?php echo $passwordmail; ?>">
+        <input type="hidden"  name="email"  value="<?php echo $email_name; ?>">
+ 
+        <table summary="" border="0">
+            <tbody>
+                <tr>
+                    <td colspan=2>
+                        Your account:  <?php echo $email; ?><br /><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="title">
+                        <label for="newpass">Your new password</label>
+                    </td>
+                    <td>
+                        <input name="newpass" id="newpass" type="password" value="<?php echo $newpass; ?>" /></td>
+                </tr>
+                <tr>
+                    <td class="title">
+                        <label for="newpass2">Confirm new password</label>
+                    </td>
+                    <td>
+                        <input name="newpass2" id="newpass2" type="password" value="<?php echo $newpass2; ?>" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+ 
+        <p style="text-align:center;"><input type="submit" class="button mainaction" value="Send" /></p>
+    </form>
+<?php endif; ?>
+    </div>
 </div>
 </body>
 </html>
