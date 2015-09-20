5deaab5c8a2ddf46f3249381c72cea3d7a096221
alxfzr <alxfzr@users.noreply.github.com>
2015-09-20 19:41:27 +0200

    Update config_sample.php
    https://www.ovh.com/fr/g1474.mutualise_generalite_mail_mutualise_ovh

--- config_sample.php
+++ config_sample.php
@@ -2,6 +2,6 @@
 $nic = "xxxxx-ovh"; // Votre nic ovh
 $pass = "********"; // Votre mot de passe ovh
 $domain = "yourdomaine.tld"; // your domain name
-$serveur="pop3.". $domain;
+$serveur="SSL0.OVH.NET"; 
 $dom = "@". $domain;
 ?>
